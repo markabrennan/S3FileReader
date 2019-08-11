869019d3bd63d9a184070685b65f641cef864763
markabrennan <mark.brennan@me.com>
2019-08-11 11:46:49 -0400

    2nd rev to clean up comments and add default bucket param.

--- s3filereader.py
+++ s3filereader.py
@@ -4,25 +4,27 @@ import boto3
 class S3FileReader:
     """
     class S3FileReader:
-    Class to encapsulate boto3 calls to access
-    an S3 resource and allow clients to stream
-    the contents of the file iteratively, via a
-    generator function: __iter__()
+    Class to encapsulate boto3 calls to access an S3 resource
+    and allow clients to stream the contents of the file iteratively,
+    via a generator function: __iter__()
     """
 
-    def __init__(self, cfg, bucket, resource_key):
+    def __init__(self, cfg, resource_key, bucket=None):
         """
         __init__(self, cfg, bucket, resource_name):
-        S3FileReader constructor initializes the S3
-        Session, gets the resource for a given bucket
-        and key, obtains the resource's object,
-        and obtains a handle to the object.
+        S3FileReader constructor initializes the S3 Session,
+        gets the resource for a given bucket and key,
+        obtains the resource's object, and obtains a handle to the object.
         Params:
             cfg: config.py file containing S3 crexentials
             bucket: name of the S3 bucket to access
             resource_key: key of the S3 resource (file name)
         """
+
         try:
+            if not bucket:
+                bucket = cfg.bucket
+
             self._session = boto3.Session(
                 aws_access_key_id=cfg.aws_access_key_id,
                 aws_secret_access_key=cfg.aws_secret_access_key)
@@ -37,15 +39,16 @@ class S3FileReader:
     def __iter__(self):
         """
         __iter__(self):
-        Provide iteration interface to clients. Get the
-        stream of our S3 object handle and produce results lazily
-        for our clients from a generator function.
+        Provide iteration interface to clients. Get the stream of our
+        S3 object handle and produce results lazily for our clients
+        from a generator function.
 
         yield statement yields a single line from the file.
 
         Returns:  nothing.  A StopIteration exception is implicitly
         raised following the completion of the for loop.
         """
+
         if not self._handle:
             raise S3FileReaderException('No S3 object handle!')
 
@@ -54,9 +57,23 @@ class S3FileReader:
             yield line
 
     def __enter__(self):
+        """
+        __enter__(self):
+        Implement Python's context management protocol
+        so this class can be used in a "with"  statement.
+        """
+
         return self
 
     def __exit__(self, exc_type, exc_value, exc_tb):
+        """
+        __exit__(self, exc_type, exc)_value, exc_tb):
+        Implement Python's context management protocol
+        so this class can be used in a "with" statement.
+        If exc_type is not None, then we are handling an
+        exception and for safety should delete our resources
+        """
+
         if exc_type is not None:
             del self._session
             del self._resource
@@ -65,15 +82,16 @@ class S3FileReader:
 
             return False
 
-        else:
+        else:  # normal exit flow
             return True
 
 
 class S3FileReaderException(Exception):
     """
-    class S#FileReaderException(Exception):
-    Simple exception class to use if we can't get an S#
-    File handle.
+    class S3FileReaderException(Exception):
+    Simple exception class to use if we can't get an S3
+    File handle, or otherwise have an exception when
+    dealing with S3.
     """
 
     def __init(self, msg):
