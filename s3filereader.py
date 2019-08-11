6e6d82c94e62d85d0d758a170d6b8a07eb11917a
markabrennan <mark.brennan@me.com>
2019-08-10 21:32:21 -0400

    added exception handling and context mgmt.

--- s3filereader.py
+++ s3filereader.py
@@ -9,6 +9,7 @@ class S3FileReader:
     the contents of the file iteratively, via a
     generator function: __iter__()
     """
+
     def __init__(self, cfg, bucket, resource_key):
         """
         __init__(self, cfg, bucket, resource_name):
@@ -21,13 +22,17 @@ class S3FileReader:
             bucket: name of the S3 bucket to access
             resource_key: key of the S3 resource (file name)
         """
-        self._session = boto3.Session(
-            aws_access_key_id=cfg.aws_access_key_id,
-            aws_secret_access_key=cfg.aws_secret_access_key)
+        try:
+            self._session = boto3.Session(
+                aws_access_key_id=cfg.aws_access_key_id,
+                aws_secret_access_key=cfg.aws_secret_access_key)
 
-        self._resource = self._session.resource('s3')
-        self._object = self._resource.Object(bucket, resource_key)
-        self._handle = self._object.get()
+            self._resource = self._session.resource('s3')
+            self._object = self._resource.Object(bucket, resource_key)
+            self._handle = self._object.get()
+
+        except Exception:
+            raise S3FileReaderException('Failed to initialize S3 resources!')
 
     def __iter__(self):
         """
@@ -41,6 +46,35 @@ class S3FileReader:
         Returns:  nothing.  A StopIteration exception is implicitly
         raised following the completion of the for loop.
         """
+        if not self._handle:
+            raise S3FileReaderException('No S3 object handle!')
+
         stream = self._handle['Body']
         for line in stream:
             yield line
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if exc_type is not None:
+            del self._session
+            del self._resource
+            del self._object
+            del self._handle
+
+            return False
+
+        else:
+            return True
+
+
+class S3FileReaderException(Exception):
+    """
+    class S#FileReaderException(Exception):
+    Simple exception class to use if we can't get an S#
+    File handle.
+    """
+
+    def __init(self, msg):
+        self.msg = msg
